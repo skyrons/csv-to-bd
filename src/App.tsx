2c3abbe1acd5b992f6ea7ea8499c251ca95c28c6
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-08-18 00:59:40 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
 import { useEffect } from "react"
+import { database } from "./services/firebase";
+import { push, ref, set } from "firebase/database";
+
+const pasrseCSV = (text) => {
+  const result = {
+    header: [],
+    data: [],
+  }
+
+  const [header, ...content] = text.split('\n');
+ 
+
+  result.header = header.split(',');
+  
+  content.forEach((item: string) => {
+    result.data.push(item.split(','));
+  });
+
+
+  console.log(result)
+
+  return result;
+
+};
+
+
+async function handleSendCsvToDatabase(){
+  const csvRef = ref(database, 'items');
+  
+  const firebaseCsv = await push(csvRef);
+  set(firebaseCsv, {
+  })
+}
+
+
 
 export default function App() {
   useEffect(() => {
     fetch('/endure-data.csv')
       .then((r) => r.text())
       .then((text) => {
-        console.log(text);
+        (pasrseCSV(text));
       });
   }, []);
-  
+
   return (
-    <div></div>
+    <div className="App">
+    </div>
   )
 }
