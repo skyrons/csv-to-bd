83362a03cd74bdc07a11a5585abd310276cd7033
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-08-18 13:10:05 -0300

    CSV reader input data to firebase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,53 @@
-import { useEffect } from "react"
+import { useState } from "react"
 import { database } from "./services/firebase";
 import { push, ref, set } from "firebase/database";
-
-const pasrseCSV = (text) => {
-  const result = {
-    header: [],
-    data: [],
-  }
-
-  const [header, ...content] = text.split('\n');
- 
-
-  result.header = header.split(',');
-  
-  content.forEach((item: string) => {
-    result.data.push(item.split(','));
-  });
-
-
-  console.log(result)
-
-  return result;
-
-};
-
-
-async function handleSendCsvToDatabase(){
-  const csvRef = ref(database, 'items');
-  
-  const firebaseCsv = await push(csvRef);
-  set(firebaseCsv, {
-  })
-}
-
+import CSVReader from 'react-csv-reader'
 
 
 export default function App() {
-  useEffect(() => {
-    fetch('/endure-data.csv')
-      .then((r) => r.text())
-      .then((text) => {
-        (pasrseCSV(text));
-      });
-  }, []);
+
+  const pasrseCSV = (text) => {
+    const result = {
+      header: [],
+      data: [],
+    }
+  
+    const [header, ...content] = text;
+   
+  
+    result.header = header;
+    
+    content.forEach((item: string) => {
+      result.data.push(item);
+    });
+  
+  
+    console.log(result);
+  
+    const csvRef = ref(database, 'items');
+    
+    const firebaseCsv = push(csvRef);
+    set(firebaseCsv, {
+      header: result.header,
+      data: result.data
+    })
+  
+    return result;
+  
+  };
+
+  const [ file, setFile ] = useState ([])
+
+
+  function handleReciveFIle (data){
+    setFile(data);
+  }
 
   return (
     <div className="App">
+      <label >Add CSV file</label>
+      <CSVReader onFileLoaded={(data) => handleReciveFIle(data)} />
+      <button onClick={ () => pasrseCSV(file) }>Send</button>
     </div>
   )
 }
